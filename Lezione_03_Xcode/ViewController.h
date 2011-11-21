3a08d97da58ee0df8e26482d96f8519b18c9bf02
paska <alberto.pasca@gmail.com>
2011-11-21 10:03:34 +0100

    first

---
+++ Lezione_03_Xcode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lezione_03_Xcode
+//
+//  Created by Alberto Pasca on 19/11/11.
+//  Copyright (c) 2011 Cutaway SRL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Lezione_03_Xcode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lezione_03_Xcode
+//
+//  Created by Alberto Pasca on 19/11/11.
+//  Copyright (c) 2011 Cutaway SRL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
